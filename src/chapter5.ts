42866ec5fa4adaccfbe382d17948e4c35be72590
kawasima <kawasima1016@gmail.com>
2023-02-03 14:30:53 +0900

    Add chapter5, chapter6

---
+++ src/chapter5.ts
@@ -0,0 +1,126 @@
+import z from 'zod'
+import { match } from 'ts-pattern'
+
+// 
+// Modeling Simple Values
+//
+const CustomerId = z.number().int().positive()
+type CustomerId = z.infer<typeof CustomerId>
+
+const WidgetCode = z.string()
+type WidgetCode = z.infer<typeof WidgetCode>
+
+const UnitQuantity = z.number().int().positive()
+type UnitQuantity = z.infer<typeof UnitQuantity>
+
+const KilogramQuantity = z.number().positive()
+type KilogramQuantity = z.infer<typeof KilogramQuantity>
+
+const OrderId = z.number().int().positive()
+type OrderId = z.infer<typeof OrderId>
+
+const customerId = CustomerId.parse(42)
+const orderId = OrderId.parse(42)
+
+// ここがF#と違いtrueになってしまう。
+console.log(customerId === orderId)
+
+// 引数の取り違えも防げない
+const processCustomerId = (id: CustomerId) => {}
+processCustomerId(orderId)
+
+
+// 
+// Modeling Complex Data
+//
+
+// まだ分かっていない型は、Undefinedにしておく
+const Undefined = z.undefined()
+const CustomerInfo = Undefined
+const ShippingAddress = Undefined
+const BillingAddress = Undefined
+const OrderLine = Undefined
+const BillingAmount = Undefined
+
+const Order = z.object({
+    customerInfo: CustomerInfo,
+    shippingAddress: ShippingAddress,
+    billingAddress: BillingAddress,
+    orderLines: z.array(OrderLine),
+    amountToBill: BillingAmount
+})
+
+// Choice型のモデリング
+/*
+    data ProductCode =
+        WidgetCode
+        OR GizmoCode
+
+    data OrderQuantity =
+        UnitQuantity
+        OR KilogramQuantity
+*/
+const ProductCode = z.union([
+    z.literal("Widget"),
+    z.literal("Gizmo"),
+])
+
+const OrderQuantity = z.union([
+    z.literal("Unit"),
+    z.literal("Kilogram"),
+])
+
+//
+// Modeling Workflows with Functions
+//
+const UnvalidatedOrder = Undefined
+type UnvalidatedOrder = z.infer<typeof UnvalidatedOrder>
+const ValidatedOrder = Undefined
+type ValidatedOrder = z.infer<typeof ValidatedOrder>
+type ValidateOrder = (unvalidatedOrder: UnvalidatedOrder) => ValidateOrder
+
+const AcknowledgmentSent = Undefined
+const OrderPlaced = Undefined
+const BillableOrderPlaced = Undefined
+const PlaceOrderEvents = z.object({
+    acknowledgementSent: AcknowledgmentSent,
+    orderPlaced: OrderPlaced,
+    billableOrderPlaced: BillableOrderPlaced,
+})
+type PlaceOrderEvents = z.infer<typeof PlaceOrderEvents>
+
+// 注文受付全体を表すFunction
+type PlaceOrder = (unvalidatedValidatedOrder: UnvalidatedOrder) => PlaceOrderEvents
+
+/*
+​ 	workflow "Categorize Inbound Mail" =
+​ 	    input: Envelope contents
+​ 	    output:
+​ 	        QuoteForm (put on appropriate pile)
+​ 	        OR OrderForm (put on appropriate pile)
+​ 	        OR ...
+*/
+const EnvelopeContents = z.string()
+type EnvelopeContents = z.infer<typeof EnvelopeContents>
+const QuoteForm = Undefined
+const OrderForm = Undefined
+type OrderForm = z.infer<typeof OrderForm>
+const CategorizedMail = z.union([
+    QuoteForm,
+    OrderForm,
+])
+type CategorizedMail = z.infer<typeof CategorizedMail>
+type CategorizeInboundMail = (envelopContents: EnvelopeContents) => CategorizeInboundMail
+
+/*
+​ 	"Calculate Prices" =
+​ 	    input: OrderForm, ProductCatalog
+​ 	    output: PricedOrder
+*/
+const ProductCatalog = Undefined
+type ProductCatalog = z.infer<typeof ProductCatalog>
+const PricedOrder = Undefined
+type PricedOrder = z.infer<typeof PricedOrder>
+type CalculatePrices = (orderForm: OrderForm, productCatalog: ProductCatalog) =>
+    PricedOrder
+
---
+++ src/chapter6.ts
@@ -0,0 +1,19 @@
+import z from 'zod'
+import { match } from 'ts-pattern'
+
+//
+// The integrity of Simple Value
+//
+
+/*
+​ 	​type​ WidgetCode = WidgetCode ​of​ ​string​   ​// starting with "W" then 4 digits​
+​ 	​type​ UnitQuantity = UnitQuantity ​of​ ​int​  ​// between 1 and 1000​
+​ 	​type​ KilogramQuantity = KilogramQuantity ​of​ decimal ​// between 0.05 and 100.00
+*/
+const UnitQuantity = z.number().int().min(1).max(1000)
+const result = UnitQuantity.safeParse(0)
+match(result)
+    .with({success: true}, ({ data }) => console.log(`Success. value is ${data}`))
+    .with({success:false}, ({ error }) => console.log(`Failure, Message is ${error.message}`))
+    .exhaustive()
+
